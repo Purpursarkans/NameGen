f0c564278e5f8acf96eb44a499c0e88e3fa750b5
Purpursarkans <goga.danilov10@gmail.com>
2021-08-17 14:40:40 +0400

    update

--- NameGen.cpp
+++ NameGen.cpp
@@ -4,8 +4,11 @@ int main()
 {
     char* mask[] = {"kakao","kakar","kakakao"};
 
-    char mask1[] = "test";
+    char* group[] = {"a", "q w e"};
     
+    std::cout << sizeof(*mask) << std::endl;
+
+    std::cout << mask[0] << " " << mask[1] << " " << mask[2] << std::endl;
     std::cout << "NameGen" << std::endl;
     system("pause");
 }
