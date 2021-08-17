3d76f4748fd2680c48d91aa499fa155cd14be8b1
Purpursarkans <goga.danilov10@gmail.com>
2021-08-17 12:32:55 +0000

    update

--- NameGen.cpp
+++ NameGen.cpp
@@ -1,5 +1,24 @@
 #include <bits/stdc++.h>
 
+#define PLATFORM "UNKNOWN"
+
+#ifdef _WIN64
+        #define PLATFORM "WINDOWS"
+        #define PWIN 1
+#elif __linux__
+        #define PLATFORM "LINUX"
+        #define PLIN 1
+#endif
+
+void pause()
+{
+    #ifdef PWIN
+        system("pause");
+    #elif PLIN
+        system("read -n1 -r -p \"Press any key to continue...\" key");
+    #endif
+}
+
 int main()
 {
     char* mask[] = {"kakao","kakar","kakakao"};
@@ -7,5 +26,6 @@ int main()
     char mask1[] = "test";
     
     std::cout << "NameGen" << std::endl;
-    system("pause");
+    std::cout << PLATFORM << std::endl;
+    pause();
 }
